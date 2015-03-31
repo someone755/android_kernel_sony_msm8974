b2a4aed806998906804fbc11671e64c3b09efdff
someone755 <jari.begus@hotmail.com>
2015-03-31 18:51:34 +0200

    Revert "thermal: bring in showp1984's driver"
    This reverts commit d0d1b02e97425b48351bfcac27f7b6e6ea239636.

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -212,26 +212,6 @@ static void __init msm8974_early_memory(void)
 	of_scan_flat_dt(dt_scan_for_memory_hole, msm8974_reserve_table);
 }
 
-#ifdef CONFIG_BRICKED_THERMAL
-static struct msm_thermal_data msm_thermal_pdata = {
-	.sensor_id = 0,
-	.poll_ms = 400,
-	.shutdown_temp = 83,
-
-	.allowed_max_high = 79,
-	.allowed_max_low = 74,
-	.allowed_max_freq = 300000,
-
-	.allowed_mid_high = 76,
-	.allowed_mid_low = 71,
-	.allowed_mid_freq = 960000,
-
-	.allowed_low_high = 74,
-	.allowed_low_low = 68,
-	.allowed_low_freq = 1728000,
-};
-#endif
-
 void __init msm8974_add_devices(void)
 {
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
@@ -260,11 +240,7 @@ void __init msm8974_add_drivers(void)
 	else
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
-#ifdef CONFIG_BRICKED_THERMAL
-	//msm_thermal_init(&msm_thermal_pdata);
-#else
 	msm_thermal_device_init();
-#endif
 #ifdef CONFIG_LCD_KCAL
 	add_lcd_kcal_devices();
 #endif
